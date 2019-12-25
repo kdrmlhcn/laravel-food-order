dbca19b2cb761e49644158b74d3f1af98bbd0e6d
kadirmelihcan <kadirmelih.can@clevel.team>
2019-12-25 22:52:56 +0300

    change content in about page

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -9,7 +9,7 @@
     <div class="row featurette">
         <div class="col-md-7">
             <h2 class="featurette-heading">HAKKIMIZDA</h2>
-            <p class="lead">Keban Et 1976 yılında Keban Kasap olarak kuruldu.</p>
+            <p class="lead">Keban Et 1990 yılında Keban Kasap olarak kuruldu.</p>
             <p class="lead">Butik bir kasap olmasına rağmen müşterilerine sunmuş olduğu ürünlerin katkısız ve lezzetli olmasından dolayı kısa sürede 'Et Ustası' ünvanının aldı. 41 yıldır iş hacmi, ürün yelpazesi, müşteriler, çalışanlar kısacası bir çok şey değişti. Ancak değişmeyen tek şey Keban Et'in sunduğu et ve et ürünlerinin lezzeti idi.</p>
             <p class="lead">Keban Kasap müşterilerinden aldığı talepler ile kalitesini hiç bozmadan büyüyebilen nadir et üreticilerindendir.İlerleyen yıllarda gücünü müşterilerinin sadakat ve övgülerinden alarak üretim bandını genişletti.</p>
             <p class="lead">İstanbul'un en köklü kasapları arasında yer alan Keban Kasap şu anda üretimine 1500m2 kapalı alana sahip Keban Et Entegre tesislerinde devam etmekte.</p>
